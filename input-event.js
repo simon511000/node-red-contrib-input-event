da27ee3e55517a9eea567d591511958b39e7b61e
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-07-22 17:57:19 +0200

    translate keycode to letter

--- input-event.js
+++ input-event.js
@@ -9,14 +9,14 @@ module.exports = function(RED) {
         var node = this;
 
         // Get keyboard device
-        exec("ls /dev/input/by-path/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
-            var device = "/dev/input/by-path/" + stdout.split('\n')[0];
+        exec("ls /dev/input/by-id/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
+            var device = "/dev/input/by-id/" + stdout.split('\n')[0];
 
             var input = new InputEvent(device);
             var keyboard = new InputEvent.Keyboard(input);
             keyboard.on('keypress', function(event) {
                 node.send({
-                    payload: event.code
+                    payload: String.fromCharCode(event.code)
                 });
             });
         })
--- test.js
+++
@@ -1,5 +0,0 @@
-var { exec } = require('child_process')
-
-exec("ls /dev/input/by-path/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
-    console.log(stdout.split('\n')[0])
-})

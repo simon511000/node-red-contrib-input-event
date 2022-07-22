43c3c0d48b4c7531e0d06a26e97da1ab200832b7
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-07-22 19:24:24 +0200

    use uiohook instead of input-event

--- input-event.js
+++ input-event.js
@@ -1,25 +1,17 @@
 module.exports = function(RED) {
     "use strict";
-    var InputEvent = require('input-event');
-    var { exec } = require('child_process')
+    var { uIOhook } = require('uiohook-napi');
     
     function KeyPressNode(config) {
         RED.nodes.createNode(this,config);
         this.device = config.device;
         var node = this;
 
-        // Get keyboard device
-        exec("ls /dev/input/by-path/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
-            var device = "/dev/input/by-path/" + stdout.split('\n')[0];
-
-            var input = new InputEvent(device);
-            var keyboard = new InputEvent.Keyboard(input);
-            keyboard.on('keypress', function(event) {
-                node.send({
-                    payload: String.fromCharCode(event.code)
-                });
+        uIOhook.on('keydown', (e) => {
+            node.send({
+                payload: e.keycode
             });
-        })
+        });
     }
 
     RED.nodes.registerType("key-press", KeyPressNode);

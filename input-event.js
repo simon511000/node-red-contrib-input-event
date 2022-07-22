6ae9901c0aff1489a9e36e71bbba9d2af9ee8885
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-07-22 12:05:10 +0200

    test

--- input-event.js
+++ input-event.js
@@ -1,24 +1,25 @@
-console.log("Si là ça marche pas, je comprend rien...");
-
 module.exports = function(RED) {
     "use strict";
     var InputEvent = require('input-event');
-
-    console.log("heuuu non ?");
+    var { exec } = require('child_process')
     
     function KeyPressNode(config) {
         RED.nodes.createNode(this,config);
         this.device = config.device;
         var node = this;
-        console.log("device : " + node.device)
-        var input = new InputEvent(node.device);
-        var keyboard = new InputEvent.Keyboard(input);
-        keyboard.on('keypress', function(event) {
-            console.log("keypress!!")
-            node.send({
-                payload: event.code
+
+        // Get keyboard device
+        exec("ls /dev/input/by-path/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
+            var device = stdout.split('\n')[0];
+
+            var input = new InputEvent(device);
+            var keyboard = new InputEvent.Keyboard(input);
+            keyboard.on('keypress', function(event) {
+                node.send({
+                    payload: event.code
+                });
             });
-        });
+        })
     }
 
     RED.nodes.registerType("key-press", KeyPressNode);

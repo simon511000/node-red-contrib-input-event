bbee4b5eaa28bae1d820b24240b45e6828066e6c
ledo0024 <simon.ledoux@etudiant.univ-reims.fr>
2022-07-23 12:00:26 +0200

    fix crash

--- input-event.js
+++ input-event.js
@@ -1,24 +1,28 @@
 module.exports = function(RED) {
     "use strict";
     var InputEvent = require('input-event');
-    var { exec } = require('child_process')
+    var fs = require('fs');
     
     function KeyPressNode(config) {
         RED.nodes.createNode(this,config);
         this.device = config.device;
         var node = this;
 
-        // Get keyboard device
-        exec("ls /dev/input/by-path/ | grep -m 1 'kbd'", function(error, stdout, stderr) {
-            var device = "/dev/input/by-path/" + stdout.split('\n')[0];
-
-            var input = new InputEvent(device);
-            var keyboard = new InputEvent.Keyboard(input);
-            keyboard.on('keypress', function(event) {
-                node.send({
-                    payload: event.code
+        fs.readdir('/dev/input/by-path/', (err, files) => {
+            // Keep only keyboards
+            var keyboards = files.filter((file) => file.includes('kbd'));
+        
+            if(keyboards.length > 0) {
+                var keyboard = keyboards[0];
+        
+                var input = new InputEvent('/dev/input/by-path/' + keyboard);
+                var keyboard = new InputEvent.Keyboard(input);
+                keyboard.on('keypress', function(event) {
+                    node.send({
+                        payload: event.code
+                    });
                 });
-            });
+            }
         });
     }
 
